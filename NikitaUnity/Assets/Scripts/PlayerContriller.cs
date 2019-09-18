1a93a1cf1d65ceed5f73aa495d9d211c1f302f6e
Aptecc08 <Nikit0chka@mail.ru>
2019-09-18 19:02:33 +0700

    NikitaUnity

--- NikitaUnity/Assets/Scripts/PlayerContriller.cs
+++ NikitaUnity/Assets/Scripts/PlayerContriller.cs
@@ -6,37 +6,38 @@ public class PlayerContriller : MonoBehaviour
 {
     public GameObject player;
     public int speed = 5;
-    public int vect;
+    public bool right;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        right = true;
     }
 
     // Update is called once per frame 
     void Update()
     {
-    player.transform.Rotate(0, 0, 1000 * Time.deltaTime);
-    player.transform.position += transform.forward * speed * Time.deltaTime;
-    vect = 1;
-    if (Input.GetMouseButtonDown(1))
+    if (right == true)
+        {
+            player.transform.Rotate(0, 0, 100 * Time.deltaTime);
+            player.transform.position += transform.forward* speed * Time.deltaTime;
+        }
+    else
+        {
+            player.transform.Rotate(0, 0, 100 * Time.deltaTime);
+            player.transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetMouseButtonDown(1))
             click();
     }
 
     public void click()
     {
-        if (vect == 1)
-        {
-            player.transform.position += transform.up * speed * Time.deltaTime;
-            vect = 0;
-        }
-        else if (vect == 0)
-        {
-            player.transform.position += transform.forward * speed * Time.deltaTime;
-            vect = 1;
-        }
+        if (right == true)
+            right = false;
+        else
+            right = true;
     }
 
    
