d5f233b1cf704af46a90062941c6ef0be99bb510
Aptecc08 <Nikit0chka@mail.ru>
2020-01-17 23:03:25 +0700

    NikitaU

---
+++ NikitaUnity/Assets/Scripts/Anim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    public Animation anim;
+    public GameObject empty;
+    public GameObject Trassa;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnCollisionEnter(Collision empty)
+    {
+        ContactPoint contact = empty.contacts[0];
+        anim.Play("");
+        Destroy(Trassa);
+    }
+}
